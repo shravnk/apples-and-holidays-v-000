6128cfd8554433462fe91db9508fa36d33154149
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-01 17:20:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize! + ":"
 
     season_hash.each do |holiday, supplies|
-      
+
       if holiday.to_s.include? "_"
         line_of_text << holiday.to_s.split("_").map(&:capitalize).join(" ")
       else
@@ -91,7 +91,7 @@ def all_supplies_in_holidays(holiday_hash)
       puts line_of_text
       line_of_text = "  "
     end
-    
+
   end
 end
 all_supplies_in_holidays(holiday_supplies)
