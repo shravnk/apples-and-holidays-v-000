79810a326012adac8f4c3f2020fb2cc0ef668e2c
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-01 17:01:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize!
     season_hash.each do |holiday, supplies|
       if holiday.to_s.include? "_"
-        print holiday.to_s.split("_").capitalize.join(" ")
+        print holiday.to_s.split("_").map(&:capitalize).join(" ")
       else
         print holiday.to_s.capitalize!
       end
