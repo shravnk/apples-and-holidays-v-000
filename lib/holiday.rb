2879189a19c78e15bd21d5e91c8a244c5d4a6dca
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-01 16:51:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, season_hash|
-    puts season.to_s
+    puts season.to_s.capitalize!
     season_hash.each do |holiday, supplies|
       print holiday.to_s + ": "
       print supplies.join(", ") + "\n"
